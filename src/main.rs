7b9279e803696d47bcd4fdc8e3c78559d4d79cb2
Alekssasho <aleksandar.angelovv@gmail.com>
2023-02-07 10:03:56 +0200

    Added example level and center it on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+static HALF_WIDTH_EXTENT: f32 = 1280.0 / 2.0;
+static HALF_HEIGHT_EXTENT: f32 = 768.0 / 2.0;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: HALF_WIDTH_EXTENT * 2.0,
+                height: HALF_HEIGHT_EXTENT * 2.0,
+                title: "Tower Defence".to_string(),
+                ..default()
+            },
+            ..default()
+        }))
         .add_plugin(LdtkPlugin)
         .add_startup_system(setup)
         .insert_resource(LevelSelection::Index(0))
@@ -15,7 +26,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("my_project.ldtk"),
+        ldtk_handle: asset_server.load("main_level.ldtk"),
+        transform: Transform::from_xyz(-HALF_WIDTH_EXTENT, -HALF_HEIGHT_EXTENT, 0.0),
         ..Default::default()
     });
 }
