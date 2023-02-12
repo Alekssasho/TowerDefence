2f7086ebcd7f857ef95ddf2363185a6bcabce67b
Alekssasho <aleksandar.angelovv@gmail.com>
2023-02-12 16:29:29 +0200

    Added better level with spawner and routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::entity::EntityLocation};
 use bevy_ecs_ldtk::prelude::*;
 
 static HALF_WIDTH_EXTENT: f32 = 1280.0 / 2.0;
@@ -18,7 +18,9 @@ fn main() {
         .add_plugin(LdtkPlugin)
         .add_startup_system(setup)
         .insert_resource(LevelSelection::Index(0))
-        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
+        .register_ldtk_entity::<SpawnerBundle>("Spawner")
+        .register_ldtk_entity::<TowerSlotBundle>("Tower_Slot")
+        .add_system(spawn)
         .run();
 }
 
@@ -26,15 +28,40 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("main_level.ldtk"),
+        ldtk_handle: asset_server.load("TowerDefence.ldtk"),
         transform: Transform::from_xyz(-HALF_WIDTH_EXTENT, -HALF_HEIGHT_EXTENT, 0.0),
         ..Default::default()
     });
 }
 
+fn spawn(mut commands: Commands, spawners: Query<&Transform, With<TowerSlot>>)
+{
+    for transform in spawners.iter() {
+            //println!("Spawner Position {:?}", transform.translation);
+    }
+}
+
+#[derive(Component, Default)]
+struct Spawner;
+
+
+#[derive(Component, Default)]
+struct TowerSlot;
+
 #[derive(Bundle, LdtkEntity)]
-pub struct MyBundle {
+pub struct SpawnerBundle {
     #[sprite_sheet_bundle]
     #[bundle]
     sprite_bundle: SpriteSheetBundle,
+
+    spawner: Spawner,
+}
+
+#[derive(Bundle, LdtkEntity)]
+pub struct TowerSlotBundle {
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
+
+    slot: TowerSlot,
 }
