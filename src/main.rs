f4887a9351a58fba76362bd225033fa2d8804d0b
Aleksandar Angelov <aleksandar.angelov@ubisoft.com>
2023-02-13 14:02:43 +0200

    Added loading of animation sprites and started implementing loading of the patrols

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use bevy::{prelude::*, ecs::entity::EntityLocation};
+use bevy::prelude::*;
+use bevy_aseprite::*;
 use bevy_ecs_ldtk::prelude::*;
+
+mod sprites {
+    use bevy_aseprite::aseprite;
+    aseprite!(pub SlowEnemy, "Leafbug.aseprite");
+}
 
 static HALF_WIDTH_EXTENT: f32 = 1280.0 / 2.0;
 static HALF_HEIGHT_EXTENT: f32 = 768.0 / 2.0;
@@ -16,10 +22,12 @@ fn main() {
             ..default()
         }))
         .add_plugin(LdtkPlugin)
+        .add_plugin(AsepritePlugin)
         .add_startup_system(setup)
         .insert_resource(LevelSelection::Index(0))
         .register_ldtk_entity::<SpawnerBundle>("Spawner")
         .register_ldtk_entity::<TowerSlotBundle>("Tower_Slot")
+        .register_ldtk_entity::<PatrolRoutePointBundle>("Patrol_Route_Point")
         .add_system(spawn)
         .run();
 }
@@ -27,10 +35,19 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
+    // Load Level
     commands.spawn(LdtkWorldBundle {
         ldtk_handle: asset_server.load("TowerDefence.ldtk"),
         transform: Transform::from_xyz(-HALF_WIDTH_EXTENT, -HALF_HEIGHT_EXTENT, 0.0),
         ..Default::default()
+    });
+
+    // Load sprite animations
+    commands.spawn(AsepriteBundle {
+        aseprite: asset_server.load(sprites::SlowEnemy::PATH),
+        animation: anim::AsepriteAnimation::from(sprites::SlowEnemy::tags::MOVE),
+        transform: Transform::from_xyz(0., 0., 5.0),
+        ..default()
     });
 }
 
@@ -64,4 +81,38 @@ pub struct TowerSlotBundle {
     sprite_bundle: SpriteSheetBundle,
 
     slot: TowerSlot,
+}
+
+#[derive(Component)]
+pub struct PatrolRoutePoint {
+    next: Entity,
+}
+
+#[derive(Bundle, LdtkEntity)]
+pub struct PatrolRoutePointBundle {
+    #[ldtk_entity]
+    data: PatrolRoutePoint,
+}
+
+impl LdtkEntity for PatrolRoutePoint {
+    fn bundle_entity(
+        entity_instance: &EntityInstance,
+        layer_instance: &LayerInstance,
+        _: Option<&Handle<Image>>,
+        _: Option<&TilesetDefinition>,
+        _: &AssetServer,
+        _: &mut Assets<TextureAtlas>,
+    ) -> Self {
+        let next_patrol_field = entity_instance.field_instances.iter().find(|f| f.identifier == *"Next_Point").unwrap();
+        if let FieldValue::EntityRef(next_patrol) = &next_patrol_field.value {
+            if let Some(next) = next_patrol {
+                layer_instance.entity_instances
+            } else {
+                panic!("Patrol route without next point");
+            }
+        }
+        PatrolRoutePoint {  
+
+        }
+    }
 }
